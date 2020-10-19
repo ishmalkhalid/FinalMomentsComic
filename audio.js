3fdadc8208abef6b1cb02cf09828f838c9710e24
Matthew Varona <mjv379@nyu.edu>
2020-10-19 15:40:33 +0400

    About

--- audio.js
+++ audio.js
@@ -1,31 +1,31 @@
-$("#coffee").click(function(){
+$(".coffee").click(function(){
 	$("<audio></audio>").attr({ 
 		'src':'sounds/coffee.mp3', 
 		'volume':0.4,
 		'autoplay':'autoplay'
-	}).appendTo("body");
+	}).appendTo(".coffee");
 });
 
-$("#office").click(function(){
+$(".office").click(function(){
 	$("<audio></audio>").attr({ 
 		'src':'sounds/office.mp3', 
 		'volume':0.4,
 		'autoplay':'autoplay'
-	}).appendTo("body");
+	}).appendTo(".office");
 });
 
-$("#ambulance").click(function(){
+$(".ambulance").click(function(){
 	$("<audio></audio>").attr({ 
 		'src':'sounds/ambulance.mp3', 
 		'volume':0.4,
 		'autoplay':'autoplay'
-	}).appendTo("body");
+	}).appendTo(".ambulance");
 });
 
-$("#phone").click(function(){
+$(".phone").click(function(){
 	$("<audio></audio>").attr({ 
 		'src':'sounds/phone.mp3', 
 		'volume':0.4,
 		'autoplay':'autoplay'
-	}).appendTo("body");
+	}).appendTo(".phone");
 });
