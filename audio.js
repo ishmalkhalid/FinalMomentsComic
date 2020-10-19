111d6d8f2e418c276931f69351ff04565ae3188e
Matthew Varona <mjv379@nyu.edu>
2020-10-19 16:19:25 +0400

    Audio bug fix again

--- audio.js
+++ audio.js
@@ -1,31 +1,15 @@
-$(".coffee").click(function(){
-	$("<audio></audio>").attr({ 
-		'src':'sounds/coffee.mp3', 
-		'volume':0.4,
-		'autoplay':'autoplay'
-	}).appendTo(".coffee");
-});
-
-$(".office").click(function(){
-	$("<audio></audio>").attr({ 
-		'src':'sounds/office.mp3', 
-		'volume':0.4,
-		'autoplay':'autoplay'
-	}).appendTo(".office");
-});
-
 $(".ambulance").click(function(){
-	$("<audio></audio>").attr({ 
-		'src':'sounds/ambulance.mp3', 
-		'volume':0.4,
-		'autoplay':'autoplay'
-	}).appendTo(".ambulance");
+    var sounds = document.getElementsByTagName('audio');
+    for(let i=0; i<sounds.length; i++) sounds[i].pause();
+    document.getElementById("ambsound").play();
 });
-
-$(".phone").click(function(){
-	$("<audio></audio>").attr({ 
-		'src':'sounds/phone.mp3', 
-		'volume':0.4,
-		'autoplay':'autoplay'
-	}).appendTo(".phone");
+$(".coffee").click(function(){
+    var sounds = document.getElementsByTagName('audio');
+    for(let i=0; i<sounds.length; i++) sounds[i].pause();
+    document.getElementById("cofsound").play();
+});
+$(".office").click(function(){
+    var sounds = document.getElementsByTagName('audio');
+    for(let i=0; i<sounds.length; i++) sounds[i].pause();
+    document.getElementById("offsound").play();
 });
