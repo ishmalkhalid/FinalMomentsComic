1f30dc709a77a0c7f23c862727a47d86e4ac96c6
ishmalkhalid <ishmal.khalid@github.com>
2020-10-19 08:31:45 +0400

    updating site

--- script.js
+++ script.js
@@ -4,16 +4,26 @@ let openedbook = document.getElementById('openbook');
 let bookmark1 = document.getElementById('bookmark1');
 let bookmark2 = document.getElementById('bookmark2');
 let bookmark3 = document.getElementById('bookmark3');
-let bookmark4 = document.getElementById('bookmark4');
+
+let one = document.getElementById('one');
+let two = document.getElementById('two');
+let three = document.getElementById('three');
+
+//let bookmark4 = document.getElementById('bookmark4');
 //let bookmarks = document.getElementsByClassName('bookmark');
 
 let hideclosedbook = () => {
     closedbook.style.display = "none";
     openedbook.style.display = "block";
+    
     bookmark1.style.display = "block";
     bookmark2.style.display = "block";
     bookmark3.style.display = "block";
-    bookmark4.style.display = "block";
+    one.style.display = "block";
+    two.style.display = "block";
+    three.style.display = "block";
+
+//    bookmark4.style.display = "block";
 }
 
 closedbook.addEventListener('click', hideclosedbook)
@@ -21,10 +31,23 @@ closedbook.addEventListener('click', hideclosedbook)
 let openclosedbook = () => {
     closedbook.style.display = "block";
     openedbook.style.display = "none";
+    
     bookmark1.style.display = "none";
     bookmark2.style.display = "none";
     bookmark3.style.display = "none";
-    bookmark4.style.display = "none";
+    
+    one.style.display = "none";
+    two.style.display = "none";
+    three.style.display = "none";
+//    bookmark4.style.display = "none";
 }
 
 openedbook.addEventListener('click', openclosedbook)
+
+let video = document.getElementById("video");
+
+let playVideo = () => {
+    video.play();
+}
+
+video.addEventListener('mouseenter', playVideo);
